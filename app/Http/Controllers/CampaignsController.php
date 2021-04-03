26c7e01d59ebed1200766e75e98ee127935f1b46
Brandon Bawe <bawe.damue@gmail.com>
2021-04-03 01:56:03 +0100

    Removes route protection from Campaign Details Page.

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -11,7 +11,7 @@ class CampaignsController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('auth')->except('edit');
+        $this->middleware('auth')->except('edit', 'show');
     }
     /**
      * Display a listing of the resource.
